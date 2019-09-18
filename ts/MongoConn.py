e48348250e1ff5c03d86af4b77cb8dc94cafb00a
chenyongzhiaaron <31282518+chenyongzhiaaron@users.noreply.github.com>
2019-09-18 16:03:18 +0800

    Update MongoConn.py

--- ts/MongoConn.py
+++ ts/MongoConn.py
@@ -1,9 +1,7 @@
 import pymongo as pm
 """
 mongo里面表名备注
-visitedInfoBean 轻松借
-AdvanceVisitedInfoRecord 大王
-easy_statistic+dt(线上库) statistics_dt(mongo正式库)
+
 
 """
 class MongoDb:
@@ -15,7 +13,7 @@ class MongoDb:
         # 获取连接
         client = pm.MongoClient(host="", port="")
         # 连接数据库(statistics为数据库名)
-        db = client["easy_statistics"]
-        # 获取集合对象表的名称（visitedInfoBean+日期（分区格式））
+        db = client["数据库名"]
+        # 获取集合对象表的名称（表名+日期（分区格式））
         my_table = db[tablename + dt]
         return my_table
